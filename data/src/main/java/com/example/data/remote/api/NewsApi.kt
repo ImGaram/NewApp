997fa14dad8103e3b6ebe310f520d7425391d267
imgaram <s21049@gsm.hs.kr>
2022-04-13 11:39:49 +0900

    [CREATE] News Api class

--- data/src/main/java/com/example/data/remote/api/NewsApi.kt
+++ data/src/main/java/com/example/data/remote/api/NewsApi.kt
@@ -1,9 +1,12 @@
 package com.example.data.remote.api
 
+import com.example.data.remote.model.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsApi {
-//    @GET("")
-//    suspend fun getNews():Response<>
+    // https://newsapi.org/v2/top-headlines?country=kr&apiKey=709b04335e404b30a64045caf1d2dfde
+    @GET("v2/top-headlines?country=kr")
+    suspend fun getNews(@Query("apikey") apiKey: String):Response<List<NewsResponse>>
 }
