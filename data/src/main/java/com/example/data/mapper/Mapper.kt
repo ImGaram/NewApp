0722f5c41ab974a05f6496571872604358118d99
imgaram <s21049@gsm.hs.kr>
2022-04-13 19:51:37 +0900

    [CREATE] mapper class

---
+++ data/src/main/java/com/example/data/mapper/Mapper.kt
@@ -0,0 +1,25 @@
+package com.example.data.mapper
+
+import com.example.data.remote.model.NewsResponse
+
+// domain layer는 data layer를 모르기 때문에 data에서 domain layer의 response data class로 자료형을 변환해 보내줘야 한다
+object Mapper {
+    fun mapperNews(response: List<NewsResponse>?): List<NewsResponse>? {
+        return if (response != null) {
+            response.toDomain()
+        } else null
+    }
+
+    fun List<NewsResponse>.toDomain(): List<NewsResponse> {
+        return this.map {
+            NewsResponse(
+                it.author,
+                it.title,
+                it.description,
+                it.url,
+                it.urlToImage,
+                it.publishedAt
+            )
+        }
+    }
+}
