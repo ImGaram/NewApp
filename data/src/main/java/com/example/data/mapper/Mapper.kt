5817f17e2676e761420a2e06b7484ba4565f6309
imgaram <s21049@gsm.hs.kr>
2022-04-15 21:25:14 +0900

    [UPDATE] mapper 수정

--- data/src/main/java/com/example/data/mapper/Mapper.kt
+++ data/src/main/java/com/example/data/mapper/Mapper.kt
@@ -1,25 +1,20 @@
 package com.example.data.mapper
 
-import com.example.data.remote.model.NewsResponse
+import com.example.data.remote.model.DataNewsResponse
+import com.example.domain.model.DomainNewsResponse
 
 // domain layer는 data layer를 모르기 때문에 data에서 domain layer의 response data class로 자료형을 변환해 보내줘야 한다
 object Mapper {
-    fun mapperNews(response: List<NewsResponse>?): List<com.example.domain.model.NewsResponse>? {
-        return if (response != null) {
-            response.toDomain()
-        } else null
-    }
-
-    fun List<NewsResponse>.toDomain(): List<com.example.domain.model.NewsResponse> {
-        return this.map {
-            com.example.domain.model.NewsResponse(
-                it.author,
-                it.title,
-                it.description,
-                it.url,
-                it.urlToImage,
-                it.publishedAt
+    fun mapperNews(responseData: DataNewsResponse?): DomainNewsResponse? {
+        return if (responseData != null) {
+            DomainNewsResponse(
+                author = responseData.author,
+                title = responseData.title,
+                description = responseData.description,
+                url = responseData.url,
+                urlToImage = responseData.urlToImage,
+                publishedAt = responseData.publishedAt
             )
-        }
+        } else responseData
     }
 }
