6aa7b1f76c0fbadd9943e71a39824e3f50114c04
imgaram <s21049@gsm.hs.kr>
2022-04-15 21:26:42 +0900

    [UPDATE] api interface 수정

--- data/src/main/java/com/example/data/remote/api/NewsApi.kt
+++ data/src/main/java/com/example/data/remote/api/NewsApi.kt
@@ -1,6 +1,6 @@
 package com.example.data.remote.api
 
-import com.example.data.remote.model.NewsResponse
+import com.example.data.remote.model.DataNewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,5 +8,5 @@ import retrofit2.http.Query
 interface NewsApi {
     // https://newsapi.org/v2/top-headlines?country=kr&apiKey=709b04335e404b30a64045caf1d2dfde
     @GET("v2/top-headlines?country=kr")
-    suspend fun getNews(@Query("apikey") apiKey: String):Response<List<NewsResponse>>
+    suspend fun getNews(@Query("apikey") apiKey: String):Response<DataNewsResponse>
 }
