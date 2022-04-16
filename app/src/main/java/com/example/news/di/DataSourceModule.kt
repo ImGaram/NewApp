285ed3df1e415e2741849d18a4fa5f82204decc5
imgaram <s21049@gsm.hs.kr>
2022-04-16 14:27:01 +0900

    [UPDATE] 이름 변경

--- app/src/main/java/com/example/news/di/DataSourceModule.kt
+++ app/src/main/java/com/example/news/di/DataSourceModule.kt
@@ -1,6 +1,5 @@
 package com.example.news.di
 
-import com.example.data.remote.api.NewsApi
 import com.example.data.repository.remote.datasource.NewsDataSource
 import com.example.data.repository.remote.datasourceimpl.NewsDataSourceImpl
 import dagger.Module
@@ -11,13 +10,13 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataSourcelmplModule {
+class DataSourceModule {
 
     @Provides
     @Singleton
     fun provideMainDataSource(
-        newsApi: NewsApi
+        newsDataSourceImpl: NewsDataSourceImpl
     ): NewsDataSource {
-        return NewsDataSourceImpl(newsApi)
+        return newsDataSourceImpl
     }
 }
