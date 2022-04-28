c13eb7ab2ace0413c899969f7a8e3abe56a9cb91
imgaram <s21049@gsm.hs.kr>
2022-04-28 19:46:22 +0900

    [CREATE] mapper, useCase file

--- data/src/main/java/com/example/data/mapper/Mapper.kt
+++ data/src/main/java/com/example/data/mapper/Mapper.kt
@@ -5,16 +5,15 @@ import com.example.domain.model.DomainNewsResponse
 
 // domain layer는 data layer를 모르기 때문에 data에서 domain layer의 response data class로 자료형을 변환해 보내줘야 한다
 object Mapper {
-    fun mapperNews(responseData: DataNewsResponse?): DomainNewsResponse? {
-        return if (responseData != null) {
-            DomainNewsResponse(
-                author = responseData.author,
-                title = responseData.title,
-                description = responseData.description,
-                url = responseData.url,
-                urlToImage = responseData.urlToImage,
-                publishedAt = responseData.publishedAt
-            )
-        } else responseData
+    fun DataNewsResponse.mapperNews(): DomainNewsResponse {
+        return DomainNewsResponse(
+            this._author,
+            this._title,
+            this._description,
+            this._url,
+            this._urlToImage,
+            this._publishedAt
+        )
     }
+
 }
--- domain/src/main/java/com/example/domain/usecase/GetNewsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetNewsUseCase.kt
@@ -1,12 +1,29 @@
 package com.example.domain.usecase
 
+import com.example.domain.model.DomainNewsResponse
 import com.example.domain.repository.NewsRepository
-import com.example.domain.utils.RemoteErrorEmitter
+import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 // repository를 hilt를 이용해 주입받아 하나의 하나의 세부적인 기능을 선언한다
 class GetNewsUseCase @Inject constructor(
     private val newsRepository: NewsRepository
 ) {
-    suspend fun execute(remoteErrorEmitter: RemoteErrorEmitter, apiKey: String) = newsRepository.getNews(remoteErrorEmitter, apiKey)
+    operator fun invoke(
+        apiKey: String,
+        scope: CoroutineScope,
+        onResult: (List<DomainNewsResponse>) -> Unit = {}
+    ) {
+        scope.launch(Dispatchers.Main) {
+            val deferred = async(Dispatchers.IO) {
+                newsRepository.getNews(apiKey)
+            }
+            onResult(deferred.await())
+        }
+    }
+    suspend fun execute(apiKey: String) = newsRepository.getNews(apiKey)
 }
