e39fb4208ff051db1f1db07a0b5aef1d863c21fd
imgaram <s21049@gsm.hs.kr>
2022-04-28 19:43:04 +0900

    [DELETE] 삭제

--- domain/src/main/java/com/example/domain/utils/ErrorType.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.utils
-
-// api 호출 에러 타입 class
-enum class ErrorType {
-    NETWORK,
-    TIMEOUT,
-    SESSION_EXPIRED,
-    UNKNOWN
-}
--- domain/src/main/java/com/example/domain/utils/RemoteErrorEmitter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.domain.utils
-
-// 에러 타입과 메시지를 받기 위한 interface
-interface RemoteErrorEmitter {
-    fun onError(msg: String)
-    fun onError(errorType: ErrorType)
-}
