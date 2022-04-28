b2b18aa52fae3ce9d08b978d5907acf092de75ac
imgaram <s21049@gsm.hs.kr>
2022-04-28 19:47:20 +0900

    [CREATE] base viewModel file

--- app/src/main/java/com/example/news/base/BaseViewModel.kt
+++ app/src/main/java/com/example/news/base/BaseViewModel.kt
@@ -1,25 +1,7 @@
 package com.example.news.base
 
-import android.view.View
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.domain.utils.ErrorType
-import com.example.domain.utils.RemoteErrorEmitter
-import com.example.domain.utils.ScreenState
-import com.example.news.widget.utils.SingleLiveEvent
 
-abstract class BaseViewModel: ViewModel(), RemoteErrorEmitter {
-    val mutableProgress = MutableLiveData<Int>(View.GONE)
-    val mutableScreenState = SingleLiveEvent<ScreenState>()
-    val mutableErrorMessage = SingleLiveEvent<String>()
-    val mutableSuccessMessage = MutableLiveData<String>()
-    val mutableErrorType = SingleLiveEvent<ErrorType>()
+abstract class BaseViewModel: ViewModel() {
 
-    override fun onError(errorType: ErrorType) {
-        mutableErrorType.postValue(errorType)
-    }
-
-    override fun onError(msg: String) {
-        mutableErrorMessage.postValue(msg)
-    }
 }
