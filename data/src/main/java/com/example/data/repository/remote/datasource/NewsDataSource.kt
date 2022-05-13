a654d7d8ed272e2ce527db2e20f325e64f44ad25
imgaram <s21049@gsm.hs.kr>
2022-05-13 20:44:33 +0900

    news data source 파일 수정

--- data/src/main/java/com/example/data/repository/remote/datasource/NewsDataSource.kt
+++ data/src/main/java/com/example/data/repository/remote/datasource/NewsDataSource.kt
@@ -1,10 +1,12 @@
 package com.example.data.repository.remote.datasource
 
 import com.example.data.remote.model.DataNewsResponse
+import com.example.data.remote.model.NewsResponseFirst
 import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
 
 interface NewsDataSource {
     suspend fun getNews(
         apiKey: String
-    ): List<DataNewsResponse>
+    ): NewsResponseFirst?
 }
--- data/src/main/java/com/example/data/repository/remote/datasourceimpl/NewsDataSourceImpl.kt
+++ data/src/main/java/com/example/data/repository/remote/datasourceimpl/NewsDataSourceImpl.kt
@@ -1,19 +1,24 @@
 package com.example.data.repository.remote.datasourceimpl
 
+import com.example.data.base.BaseDataSource
 import com.example.data.remote.api.NewsApi
 import com.example.data.remote.model.DataNewsResponse
+import com.example.data.remote.model.NewsResponseFirst
 import com.example.data.repository.remote.datasource.NewsDataSource
 import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
 import javax.inject.Inject
 
 // newsDataSource 의 구현체
 // api 호출 로직
 class NewsDataSourceImpl @Inject constructor(
-    private val newsApi: NewsApi
-): NewsDataSource {
+    override val service: NewsApi
+): BaseDataSource<NewsApi>(), NewsDataSource {
     override suspend fun getNews(
         apiKey: String
-    ): List<DataNewsResponse> {
-        return newsApi.getNews(apiKey)
+    ): NewsResponseFirst? {
+        return safeApiCall {
+            service.getNews(apiKey)
+        }?.body()
     }
 }
