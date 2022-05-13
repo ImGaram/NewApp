342ff589a0603e3eba63a41513317284723599c1
imgaram <s21049@gsm.hs.kr>
2022-05-13 20:46:37 +0900

    view model, gradle 파일 수정

--- app/src/main/java/com/example/news/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/news/viewmodel/MainViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.DomainNewsResponse
+import com.example.domain.model.DomainNewsResponseFirst
 import com.example.domain.usecase.GetNewsUseCase
 import com.example.news.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 // UseCase를 Hilt를 이용해 주입받아 사용
@@ -15,8 +17,8 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val getNewsUseCase: GetNewsUseCase
 ): BaseViewModel() {
-    private val _news =MutableLiveData<List<DomainNewsResponse>>()
-    val news: LiveData<List<DomainNewsResponse>> = _news
+    private val _news =MutableLiveData<DomainNewsResponseFirst>()
+    val news: LiveData<DomainNewsResponseFirst> = _news
 
     var apiCallResult = DomainNewsResponse("","" , "", "", "", "")
 
