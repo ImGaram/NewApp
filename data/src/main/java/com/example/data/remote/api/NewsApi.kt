9786cceafa193a2d6e275d82ece93b2c77d15066
imgaram <s21049@gsm.hs.kr>
2022-05-13 20:44:15 +0900

    news api 파일 수정

--- data/src/main/java/com/example/data/remote/api/NewsApi.kt
+++ data/src/main/java/com/example/data/remote/api/NewsApi.kt
@@ -1,13 +1,12 @@
 package com.example.data.remote.api
 
-import com.example.data.remote.model.DataNewsResponse
-import com.google.gson.annotations.SerializedName
-import io.reactivex.rxjava3.core.Single
+import com.example.data.remote.model.NewsResponseFirst
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApi {
     // https://newsapi.org/v2/top-headlines?country=kr&apiKey=709b04335e404b30a64045caf1d2dfde
     @GET("v2/top-headlines?country=kr")
-    suspend fun getNews(@Query("apikey") apiKey: String): List<DataNewsResponse>
+    suspend fun getNews(@Query("apikey") apiKey: String): Response<NewsResponseFirst>
 }
