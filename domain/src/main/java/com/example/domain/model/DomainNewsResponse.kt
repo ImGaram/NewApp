db6f1b606aedb6222b14a17be6d5dc329a710a58
imgaram <s21049@gsm.hs.kr>
2022-05-27 18:00:00 +0900

    domain response 수정

--- domain/src/main/java/com/example/domain/model/DomainNewsResponse.kt
+++ domain/src/main/java/com/example/domain/model/DomainNewsResponse.kt
@@ -3,11 +3,11 @@ package com.example.domain.model
 import com.google.gson.annotations.SerializedName
 
 data class DomainNewsResponse(
-    @SerializedName("author") var _author: String? = null,
-    @SerializedName("title") var _title: String? = null,
-    @SerializedName("description") var _description: String? = null,
-    @SerializedName("url") var _url: String? = null,
-    @SerializedName("urlToImage") var _urlToImage: String? = null,
-    @SerializedName("publishedAt") var _publishedAt: String? = null
+    @SerializedName("author") var _author: String,
+    @SerializedName("title") var _title: String,
+    @SerializedName("description") var _description: String,
+    @SerializedName("url") var _url: String,
+    @SerializedName("urlToImage") var _urlToImage: String,
+    @SerializedName("publishedAt") var _publishedAt: String
 )
 
