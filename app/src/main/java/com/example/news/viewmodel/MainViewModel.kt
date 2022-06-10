ff14331d4312aa43ff18f475f829588fe78eedaf
imgaram <s21049@gsm.hs.kr>
2022-06-10 21:32:28 +0900

    뉴스 api data view model

--- app/src/main/java/com/example/news/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/news/viewmodel/MainViewModel.kt
@@ -1,18 +1,13 @@
 package com.example.news.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.domain.model.DomainNewsAllResponse
 import com.example.domain.model.DomainNewsResponse
-import com.example.domain.model.DomainNewsResponseFirst
 import com.example.domain.usecase.GetNewsUseCase
 import com.example.news.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import okhttp3.ResponseBody
-import org.json.JSONArray
-import org.json.JSONObject
-import retrofit2.Response
 import javax.inject.Inject
 
 // UseCase를 Hilt를 이용해 주입받아 사용
@@ -24,13 +19,12 @@ class MainViewModel @Inject constructor(
 //    private val _news = MutableLiveData<List<DomainNewsResponse>>()
 //    val news: LiveData<List<DomainNewsResponse>> = _news
 
-    private val _apiCallResult = MutableLiveData<DomainNewsResponseFirst>()
-    val apiCallResult: LiveData<DomainNewsResponseFirst> = _apiCallResult
+    private val _apiCallResult = MutableLiveData<DomainNewsAllResponse>()
+    val apiCallResult: LiveData<DomainNewsAllResponse> = _apiCallResult
 
     fun getNews(apiKey: String) {
         getNewsUseCase(apiKey, viewModelScope) {
             _apiCallResult.value = it
-//            _news.value = _apiCallResult.value?.articles
         }
     }
 }
