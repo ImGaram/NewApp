0471ed09030f8c7903163fb1f79f465b993b7e0e
imgaram <s21049@gsm.hs.kr>
2022-06-10 21:32:19 +0900

    뉴스 api data use case

--- domain/src/main/java/com/example/domain/usecase/GetNewsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetNewsUseCase.kt
@@ -1,15 +1,11 @@
 package com.example.domain.usecase
 
-import com.example.domain.model.DomainNewsResponse
-import com.example.domain.model.DomainNewsResponseFirst
+import com.example.domain.model.DomainNewsAllResponse
 import com.example.domain.repository.NewsRepository
-import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import okhttp3.ResponseBody
-import retrofit2.Response
 import javax.inject.Inject
 
 // repository를 hilt를 이용해 주입받아 하나의 하나의 세부적인 기능을 선언한다
@@ -19,7 +15,7 @@ class GetNewsUseCase @Inject constructor(
     operator fun invoke(
         apiKey: String,
         scope: CoroutineScope,
-        onResult: (DomainNewsResponseFirst?) -> Unit = {}
+        onResult: (DomainNewsAllResponse?) -> Unit = {}
     ) {
         scope.launch(Dispatchers.Main) {
             val deferred = async(Dispatchers.IO) {
