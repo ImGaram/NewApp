fbe364539df245d519175fbd84cba8350cc1f301
imgaram <s21049@gsm.hs.kr>
2022-06-10 21:33:32 +0900

    뉴스 api data main activity

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -4,42 +4,42 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.domain.model.DomainNewsResponse
-import com.example.domain.model.DomainNewsResponseFirst
 import com.example.news.adapter.NewsRecyclerViewAdapter
 import com.example.news.base.BaseActivity
 import com.example.news.databinding.ActivityMainBinding
 import com.example.news.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainViewModel by viewModels<MainViewModel>()
-    private val list = mutableListOf<DomainNewsResponse>()
 
     override fun init() {
         binding.activity = this
         mainViewModel.getNews("709b04335e404b30a64045caf1d2dfde")
         subscribeToLiveData()
+        viewSetting()
     }
 
     private fun subscribeToLiveData() {
         mainViewModel.apiCallResult.observe(this) {
             Log.d("성공", "subscribeToLiveData mainViewModel.apiCallResult: $it")
-            initData(it, list)
+            initRecycler(it._articles)
         }
     }
 
-    private fun initData(data: DomainNewsResponseFirst, list: MutableList<DomainNewsResponse>) {
-        Log.d("RESULT", "total results: ${data.totalResults}")
-        for (i:Int in 0 until 20) {
-            list.add(data.articles[i])
-        }
-        (binding.recyclerviewNews.adapter as NewsRecyclerViewAdapter?)?.setNews(list)
-        initRecycler()
-    }
-
-    private fun initRecycler() {
-        binding.recyclerviewNews.adapter = NewsRecyclerViewAdapter()
+    private fun initRecycler(list: List<DomainNewsResponse>) {
+        binding.recyclerviewNews.adapter = NewsRecyclerViewAdapter(list)
         binding.recyclerviewNews.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
+    private fun viewSetting() {
+        val now = System.currentTimeMillis()
+        val mDate = Date(now)
+        val simpleDate = SimpleDateFormat("yyyy.MM.dd")
+        val getTime = simpleDate.format(mDate)
+        binding.textViewTodayDate.text = getTime
     }
 }
