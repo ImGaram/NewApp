1d550fc3c6dbcddee5d131267a0b6fc355ca7cb5
imgaram <s21049@gsm.hs.kr>
2022-06-25 20:58:43 +0900

    weather api 위도, 경도 구하기

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.news
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.MenuItem
-import androidx.activity.viewModels
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.news.base.BaseActivity
 import com.example.news.databinding.ActivityMainBinding
 import com.example.news.view.NewsFragment
 import com.example.news.view.WeatherFragment
-import com.example.news.viewmodel.MainViewModel
+import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
@@ -14,9 +23,24 @@ import java.util.*
 
 @AndroidEntryPoint
 class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null   // 현재 위치 가져오기 변수
+    private val REQUEST_PERMISSION_LOCATION = 10
+    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    private lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private var lat: Double = 0.0
+    private var lot: Double = 0.0
     override fun init() {
         binding.activity = this
         viewSetting()
+
+        mLocationRequest =  LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        if (checkPermissionForLocation(this)) {
+            startLocationUpdates()
+        }
 
         binding.bottomnavi.setOnNavigationItemSelectedListener(this)
         supportFragmentManager.beginTransaction().add(R.id.linear, NewsFragment()).commit()
@@ -30,6 +54,35 @@ class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main), B
         binding.textViewTodayDate.text = getTime
     }
 
+    private fun startLocationUpdates() {
+        //FusedLocationProviderClient의 인스턴스를 생성
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
+        // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    // 시스템으로 부터 위치 정보를 콜백으로 받음
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
+            locationResult.lastLocation
+            onLocationChanged(locationResult.lastLocation)
+        }
+    }
+
+    // 시스템으로 부터 받은 위치정보를 화면에 갱신해주는 메소드
+    fun onLocationChanged(location: Location?) {
+        mLastLocation = location!!
+        lat =  mLastLocation.latitude // 갱신 된 위도
+        lot =  mLastLocation.longitude // 갱신 된 경도
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.news -> {
@@ -37,10 +90,44 @@ class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main), B
                 return true
             }
             R.id.weather -> {
-                supportFragmentManager.beginTransaction().replace(R.id.linear, WeatherFragment()).commitAllowingStateLoss()
+                supportFragmentManager.beginTransaction().replace(R.id.linear, WeatherFragment().apply {
+                    arguments = Bundle().apply {
+                        putDouble("lat", lat)
+                        putDouble("lot", lot)
+                    }
+                }).commitAllowingStateLoss()
                 return true
             }
         }
         return false
+    }
+
+    // 위치 권한이 있는지 확인하는 메서드
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                // 권한이 없으므로 권한 요청 알림 보내기
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+                false
+            }
+        } else {
+            true
+        }
+    }
+
+    // 사용자에게 권한 요청 후 결과에 대한 처리 로직
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            } else {
+                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
+                Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
