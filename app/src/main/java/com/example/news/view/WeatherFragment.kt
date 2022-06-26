ad2ab10ce75a2f1a976754ee941a9df7acdf6e4c
imgaram <s21049@gsm.hs.kr>
2022-06-26 17:26:15 +0900

    weather recycler view 추가

--- app/src/main/java/com/example/news/view/WeatherFragment.kt
+++ app/src/main/java/com/example/news/view/WeatherFragment.kt
@@ -2,7 +2,11 @@ package com.example.news.view
 
 import android.util.Log
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.domain.model.weather.DomainWeatherListResponse
+import com.example.domain.model.weather.DomainWeatherResponse
 import com.example.news.R
+import com.example.news.adapter.WeatherWeeklyRecyclerAdapter
 import com.example.news.base.BaseFragment
 import com.example.news.databinding.FragmentWeatherBinding
 import com.example.news.viewmodel.MainViewModel
@@ -25,7 +29,13 @@ class WeatherFragment : BaseFragment<FragmentWeatherBinding>(R.layout.fragment_w
         viewModel.getWeather(lat!!, lon!!, "95dabcdab4c88faa10d407fc8d42cced")
         viewModel.weatherApiCallResult.observe(viewLifecycleOwner) {
             Log.d("SUCCESS", "getWeatherApi result: $it")
+            initWeeklyRecycler(it.list)
         }
+    }
+
+    private fun initWeeklyRecycler(item: List<DomainWeatherListResponse>) {
+        binding.weatherWeeklyRecyclerView.adapter = WeatherWeeklyRecyclerAdapter(item)
+        binding.weatherWeeklyRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     }
 
     private fun viewSetting() {
